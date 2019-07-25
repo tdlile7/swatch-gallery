ff61d4cb231a3ec98f84e2851f1c6b297049a83c
tlilecode <tlilecode@gmail.com>
2019-07-25 08:20:34 -0700

    Define PropTypes for Swatch component

--- src/components/Swatch.js
+++ src/components/Swatch.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Swatch = props => {
   const { color } = props;
@@ -15,6 +16,10 @@ const Swatch = props => {
       <div className="swatch__hex-code">{hexCode}</div>
     </div>
   );
+};
+
+Swatch.propTypes = {
+  color: PropTypes.object.isRequired
 };
 
 export default Swatch;
